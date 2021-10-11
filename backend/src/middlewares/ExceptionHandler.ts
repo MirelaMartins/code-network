c8b4a9c2c0b7078204ded4b98704d51a74344e77
eduardo-js <jose.eduardo.ds@gmail.com>
2021-10-11 15:02:49 -0300

    chore: logging unexpected errors

--- backend/src/middlewares/ExceptionHandler.ts
+++ backend/src/middlewares/ExceptionHandler.ts
@@ -1,12 +1,13 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable no-console */
 import GeneralApiError from '@/errors/GeneralApiError';
 import { NextFunction, Request, Response } from 'express';
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const ExceptionHandler = (error: Error | GeneralApiError, req: Request, res: Response, _: NextFunction): Response => {
   if (error instanceof GeneralApiError) {
     return res.status(error.status).send(error.message);
   }
-
+  console.info(`Error name: ${error.name}\nerror message: ${error.message}\nstack: ${error.stack}`);
   return res.status(500).send('Unexpected error.');
 };
 
