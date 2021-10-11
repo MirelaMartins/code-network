a78feb708d8fdd2d3be597881b1b648f0ba18cb5
eduardo-js <jose.eduardo.ds@gmail.com>
2021-10-11 13:15:46 -0300

    feat: course routes

---
+++ backend/src/repositories/Course.ts
@@ -0,0 +1,12 @@
+import Course, { ICourse } from '@/models/Course';
+import { LeanDocument } from 'mongoose';
+
+const retrieveCourse = async (id: string): Promise<LeanDocument<ICourse>> => Course.findById(id).lean().exec();
+const deleteCourse = async (id: string): Promise<LeanDocument<ICourse>> => Course.findByIdAndDelete(id).lean().exec();
+const updateCourse = async (id: string, update: Record<string, any>): Promise<any> => Course
+  . updateOne({ id }, update).lean().exec();
+const createCourse = async (data: ICourse): Promise<ICourse> => Course.create(data);
+
+export {
+  retrieveCourse, deleteCourse, createCourse, updateCourse,
+};
--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
+
 import User from './User';
+import Course from './Course';
 
 const routes = Router();
 
 routes.use('/user/', User);
+routes.use('/course/', Course);
 
 export default routes;
