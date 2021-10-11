18476afea17512f3cd16c60d11fa0c3a869386a5
eduardo-js <jose.eduardo.ds@gmail.com>
2021-10-11 13:14:03 -0300

    chore: express-async-errors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import 'express-async-errors';
 
 import ExceptionHandler from './middlewares/ExceptionHandler';
 import AuthHandler from './middlewares/Auth';
