e842443fb505832c6a8e18d36b7ae37a70a555d9
eduardo-js <jose.eduardo.ds@gmail.com>
2021-10-30 16:15:52 -0300

    fix: proper route private and public access

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,13 +1,31 @@
 import React from 'react'
+import { Route, Switch } from 'react-router-dom'
 
-import PublicRoutes from './PublicRoutes'
-import PrivateRoutes from './PrivateRoutes'
 import { useAuth } from '../contexts/Auth'
+import UrlPaths from '../enums/UrlPaths'
+import Home from '../screens/home'
+import Login from '../screens/login'
+import NotFound from '../screens/not-found'
+import RecoverPassword from '../screens/recover-password'
+import Register from '../screens/register'
+import Video from '../screens/video'
 
 const Routes: React.FC = () => {
   const { user } = useAuth()
 
-  return user ? < PrivateRoutes /> : < PublicRoutes/>
+  return (
+    <Switch>
+      <Route exact path={UrlPaths.home} component={Home} />
+      <Route exact path={UrlPaths.register} component={Register} />
+      <Route exact path={UrlPaths.login} component={Login} />
+      <Route exact path={UrlPaths.recoverPassword} component={RecoverPassword} />
+      { user &&
+      <Route exact path={UrlPaths.video} component={Video} />
+      }
+      <Route path="*" component={NotFound} />
+    </Switch>
+
+  )
 }
 
 export default Routes
