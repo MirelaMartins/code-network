f6722c867bb43f8ed17b5198ff9803ae73b0e317
eduardo-js <jose.eduardo.ds@gmail.com>
2021-10-30 15:42:33 -0300

    feat: not found screen

---
+++ frontend/src/screens/not-found/components/index.tsx
@@ -0,0 +1,14 @@
+
+import React from 'react'
+import { NotFoundStyle } from './style'
+
+const NotFoundComponent: React.FC = () => {
+  return (
+    <NotFoundStyle>
+      <p>Em construção!</p>
+      <p>Volte mais tarde.</p>
+    </NotFoundStyle>
+  )
+}
+
+export default NotFoundComponent
---
+++ frontend/src/screens/not-found/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import NotFoundComponent from './components'
+
+const NotFound = () => {
+  return (
+    <NotFoundComponent/>
+  )
+}
+
+export default NotFound
