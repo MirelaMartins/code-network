c891004f99067e7840f4a6863c2df0ff92ef72be
eduardo-js <jose.eduardo.ds@gmail.com>
2021-10-31 15:12:27 -0300

    feat: axios response interceptor & fix job url

--- frontend/src/services/Api.ts
+++ frontend/src/services/Api.ts
@@ -1,18 +1,17 @@
-import axios, { AxiosResponse } from 'axios'
-import { Course } from '../models/Course'
-import { IUser } from '../models/User'
+import axios, { AxiosError, AxiosResponse } from 'axios'
+import { IUser, ICourse, IJobOpening } from '../models'
 
 export const apiService = axios.create({
   baseURL: process.env.REACT_APP_API_URL
 })
 
+apiService.interceptors.response.use((resp: AxiosResponse) => resp.data, (err: AxiosError) => { throw new Error(err.response?.statusText) })
 class ApiService {
   static registerUser = (data: IUser) => apiService.post('user/create', data)
-  static getAllCourses = ():Promise<AxiosResponse<Course[]>> => apiService.get('course/')
-  static getAllJobs = () => apiService.get('jobs/')
+  static getAllCourses = ():Promise<ICourse[]> => apiService.get('course/')
+  static getAllJobs = (): Promise<IJobOpening[]> => apiService.get('job/')
   static patchUser = (data: Partial<IUser>) => apiService.patch('user/recover-password', data)
-
-  static login = (data: Partial<IUser>) => apiService.post('user/login', data)
+  static login = async (data: Partial<IUser>): Promise<string> => await apiService.post('user/login', data)
   static playVideo = () => apiService.post('upload/7fea670fd64912fa06dd42911ff4cc3e')
 }
 
